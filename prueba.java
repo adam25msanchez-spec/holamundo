576dd3b4cf0a607a39d65f2af872b5efa18ef7d3
Marc Sanchez <adam25msanchez@inslaferreria.cat>
2026-09-22 16:23:11 +0200

    nuevo commit archivo modificado

---
+++ prueba.java
@@ -0,0 +1,12 @@
+public double dividir(double a, double b) {
+<<<<<<< HEAD (Canvis actuals a la branca main)
+    if (b == 0) {
+        throw new IllegalArgumentException("No es pot dividir per zero");
+    }
+    return a / b;
+=======
+    // Versió de la branca feature/calculadora
+    if (b == 0.0) return 0.0;
+    return a / b;
+>>>>>>> feature/calculadora (Canvis entrant)
+}
